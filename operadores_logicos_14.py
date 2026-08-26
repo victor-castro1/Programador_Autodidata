81b5ec856c839413edaec8bd4721395d89f824f9
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-08-26 06:43:07 -0300

    add

---
+++ operadores_logicos_14.py
@@ -0,0 +1,120 @@
+# Operadores Lógicos
+#       - Operadores Lógicos != Operadores de comparação
+#       - Resultados: True (verdadeiro) e False (falso)       
+#
+#       1. Operadores lógicos
+#               
+#               a. AND (e)
+#                   - Duas expressões ou + 
+#                       - TODAS Precisam ser verdadeiras = TRUE 
+#                       - Caso contrário = False
+#               
+#                   Exemplos:.
+#                       1 == 1 AND 1 == 1 
+#                       (true)     (true)
+#                           True // resultado
+#              
+#
+#                       1 == 2 AND 2 == 2
+#                       (false)    (true)
+#                           False // resultado
+# 
+#
+#                       2 == 1 AND 3 == 2
+#                       (false)    (false)
+#                           False // Resultado
+#
+#
+#                       2 == 2 AND 4/2 == 4  
+#                       (true)     (false)
+#                           False // Resultado
+#
+#                       2 ** 4 >= 4 ** 2 AND 7/2 == 3.5 AND  10 != -1
+#                           (true)          (true)         (true)
+#                                   True // Resultado
+#
+#
+#
+#               b. OR (ou)
+#                   - Só uma expressão 
+#                       - Precisa ser verdadeira = TRUE
+#                       - Caso contrário = False
+#
+#                   Exemplos:.
+#                       1 == 1 OR 2 == 2 
+#                       (true)    (true)
+#
+#                       2 == 1 OR 2 == 2
+#                       (false)   (true)
+#
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#               c. Not (não)
+#                   - Inverte o resultado
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#       x. Diferença
+#           a. Operador Comparativo
+#                   - Análise -> entre dois variáveis 
+#                   - Ex:.
+#                           a = 5 
+#                           b = 10
+#                       
+#                           a > b // False
+#                           b >= a // True
+#                           a == b // False
+#
+#           b. Operador Lógico
+#                   - Análise -> duas condições
+#                   - Ex:.
+#                           1 == 1 and 2 == 2 
+#                         // true  and   // true
+#                               // true
+#                           
