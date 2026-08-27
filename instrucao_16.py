5a6baabbaf013e6d5db399634057e3a9341044c3
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-08-27 11:24:34 -0300

    New annotattions

---
+++ instrucao_16.py
@@ -0,0 +1,62 @@
+#   Instrução
+#       - Um Comando/ordem -> compilador -> executa 
+#
+#       Ex:.
+#           print ("Hello World!!")
+#
+#       1. Instrução Simples    
+#           - Única -> Linha de código
+#
+#           Ex:.
+#
+#               print ("Hello, Carlos!")                            # Executa diretamente 
+#   
+#               print (2+2)                                         # Pode ocupar -> única linha 
+#
+#               x = 10                                              
+#               print (x)        
+#   
+#
+#       2. Instrução Composta
+#           - Linhas de código
+#                   - Estrutura de controle -> agrupa outras intruções
+#               
+#           - Estrutura (Cláusula)
+#                   - Cabeçalho/-> Define o controle (if, try, for...)
+#                   - Bloco de código -> pertecente ao cabeçalho
+#               
+#           - Cláusula
+#                   - Cabeçalho + Bloco de código (indentado)
+#
+#                   Ex:.
+#                       Cabeçalho
+#                           conjunto de instruções
+#
+#                       Cabeçalho
+#                           instrução 1 
+#                           instrução 2 
+#                           instrução 3 
+#                           instrução 4
+#
+#            Ex:.
+#                   if idade >= 18                                      # Cabeçalho/ (define a regra / tipo de controle) -> If (Condição)
+#                       print ("Parabéns você é maior de idade")        # Bloco de código -> pertence ao cabeçalho IF (por causa da sua indentação) 
+#
+#                   
+#                   nota = 8
+#
+#                   if nota >= 9:                                       # Cláusula 1 -> if 
+#                       print ("Excelente Desempenho!")                     # Conjunto de instruções -> dentro da Cláusula 1 
+#
+#                   elif nota >= 6:                                     # Cláusula 2 -> elif
+#                       print ("Aprovado!")                                 # Conjunto de instruções -> dentro da Cláusula 2 
+#
+#                   else                                                # Cláusula 3 -> else
+#                       print ("Reprovado!")                                # Conjunto de instruções -> dentro da Cláusula 3                                
+#
+#           
+#                   
+#
+#
+#
+#
