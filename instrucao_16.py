11484243cb6834bc68496c4c1cabad4dfd370104
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-08-27 13:51:04 -0300

    Instruções

--- instrucao_16.py
+++ instrucao_16.py
@@ -18,11 +18,13 @@
 #   
 #
 #       2. Instrução Composta
+#           - Formada -> uma ou + cláusulas
+#
 #           - Linhas de código
 #                   - Estrutura de controle -> agrupa outras intruções
 #               
-#           - Estrutura (Cláusula)
-#                   - Cabeçalho/-> Define o controle (if, try, for...)
+#           - Estrutura 
+#                   - Cabeçalho/-> Define o tipo de controlador (if, try, for...)
 #                   - Bloco de código -> pertecente ao cabeçalho
 #               
 #           - Cláusula
@@ -39,10 +41,17 @@
 #                           instrução 4
 #
 #            Ex:.
-#                   if idade >= 18                                      # Cabeçalho/ (define a regra / tipo de controle) -> If (Condição)
+#
+#                   # Instrução Composta -> Contexto Idade
+#
+#                   # Cláusula (if + bloco de código)
+#                   if idade >= 18                                      # Cabeçalho (define a regra / tipo de controle) -> If (Condição)
 #                       print ("Parabéns você é maior de idade")        # Bloco de código -> pertence ao cabeçalho IF (por causa da sua indentação) 
 #
 #                   
+#
+#                   # Instrução Composta -> Contexto de Notas
+#
 #                   nota = 8
 #
 #                   if nota >= 9:                                       # Cláusula 1 -> if 
@@ -54,9 +63,13 @@
 #                   else                                                # Cláusula 3 -> else
 #                       print ("Reprovado!")                                # Conjunto de instruções -> dentro da Cláusula 3                                
 #
-#           
 #                   
-#
-#
-#
+#                   # Instrução Composta -> Contexto de acesso
+#                                 
+#                   if usuario_logado:   
+# 
+#                       # Conjunto de instruções 
+#                       print ("Bem-Vindo")                             # Instrução n°1                       
+#                       carregar_painel()                               # Instrução n°2 
+#                       mostrar_notificacoes()                          # Instrução n°3
 #
